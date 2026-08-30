a339e79f6f9119461f278707264ea47cd881d0f6
marcoramiro-create <marcoramiro@gmail.com>
2026-08-30 15:29:33 -0300

    Create config.ts

---
+++ drizzle/config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "drizzle-kit";
+
+const connectionString = process.env.SUPABASE_DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("SUPABASE_DATABASE_URL is required to run drizzle commands");
+}
+
+export default defineConfig({
+  schema: "./drizzle/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: connectionString,
+  },
+});
