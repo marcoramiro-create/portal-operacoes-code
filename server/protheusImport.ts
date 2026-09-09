1b377d3712fabd3bb8fbaf19bca5b81e4d2965c6
Marco Ramiro <marcoramiro@gmail.com>
2026-09-09 17:26:17 -0300

    corrige protheusImport (linha órfã) e cobertura

--- server/protheusImport.ts
+++ server/protheusImport.ts
@@ -26,7 +26,6 @@
  * //   As demais colunas (fórmulas do usuário e da macro) são ignoradas.
  */
 import type { PurchaseRow } from './protheusCalculations';
-const comBase = calcularCamposBase(enriquecidos, emissao)
 import { calcularCamposBase, calcularCurvasAbcde } from './protheusCalculations';
 import type { Sb1Index, SbzIndex, FamiliasMap } from './referenceImporters';
 /** Limite máximo de registros aceitos na importação de Compras. */
